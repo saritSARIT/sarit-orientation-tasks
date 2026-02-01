701bdc0d42ffe3cde34f0ae8168a0adb23427745
SARITsarit <876sarit@gmail.com>
2026-02-01 11:19:19 +0200

    Fix the backend

--- Task3/post/post.validator.ts
+++ Task3/post/post.validator.ts
@@ -1,5 +1,5 @@
 import Joi from "joi";
-import { Types } from "mongoose";
+import { Query, Types } from "mongoose";
 import type { Post } from "../types/post";
 import { maxLength } from "./post.consts";
 
@@ -7,18 +7,21 @@ const objectIdValidator = (value: string, helpers: Joi.CustomHelpers) =>
   Types.ObjectId.isValid(value) ? value : helpers.error("any.invalid");
 
 export const createPostSchema = Joi.object({
-  body: Joi.object<Omit<Post, "_id" | "likes" | "createdAt">>({
+  body:Joi.object<Omit<Post, "_id" | "createdAt">>({
     postName: Joi.string().required(),
     userId: Joi.string().custom(objectIdValidator).required(),
     text: Joi.string().max(maxLength).required(),
+    likes: Joi.number().min(0).required(),
   }).required(),
+  
 });
 
 export const updatePostSchema = Joi.object({
   body: Joi.object<Partial<Post>>({
     postName: Joi.string().optional(),
     text: Joi.string().max(maxLength).optional(),
+    likes:Joi.number().optional().min(1),
   })
-    .min(1)
     .required(),
+    params: {},
 });
--- Task3/user/user.validator.ts
+++ Task3/user/user.validator.ts
@@ -1,7 +1,9 @@
 import Joi from "joi";
 
 export const createUserSchema = Joi.object({
+  body: Joi.object({
     username: Joi.string().required(),
     displayedName: Joi.string().required(),
-  }).required();
+  }).required(),
+});
 
